5724694e5f033ffc52be85cea043a40dcd52d4f6
Constantine <qostyab@gmail.com>
2015-04-24 00:59:40 +1000

    Added history api

--- main.js
+++ main.js
@@ -6,15 +6,16 @@
 
     CardsApp.cards = data;
 
-    CardsApp.refreshViewCards = function () {
+    CardsApp.refreshViewCards = function (arg) {
+        arg = arg || data;
         CardsApp.cardsHolder.innerHTML = '';
-        for ( var i = 0; data.length > i; i++ ) {
+        for ( var i = 0; arg.length > i; i++ ) {
             var card = document.createElement('li');
-            var cardText = document.createTextNode( data[i].type );
+            var cardText = document.createTextNode( arg[i].type );
             card.appendChild( cardText );
-            card.className += 'card card-' + data[i].type;
+            card.className += 'card card-' + arg[i].type;
 
-            if ( i === data.length - 1 && data.length != 1 ) {// добавление сдвига вправо, если карточек больше 1
+            if ( i === arg.length - 1 && arg.length != 1 ) {// добавление сдвига вправо, если карточек больше 1
                 card.className += ' card-first';
             }
             this.cardsHolder.appendChild( card );
@@ -59,6 +60,7 @@
             }
         );
         this.refreshViewCards();
+        CardsApp.historyApi.add('#add');
     };
 
     CardsApp.removeCard = function () {
@@ -86,6 +88,23 @@
 
         CardsApp.refreshViewCards();
         CardsApp.hoverCards();
+        CardsApp.historyApi.add();
+        CardsApp.historyApi.detectChanges();
+    };
+
+    CardsApp.historyApi = {
+        add: function (arr) {
+            if ( window.history ) {
+                history.pushState(data, null)
+            }
+        },
+        detectChanges: function() {
+            window.addEventListener('popstate', function(e) {
+                data = e.state;
+                console.log(data);
+                CardsApp.refreshViewCards(data);
+            });
+        }
     };
 
     CardsApp.editCards();
