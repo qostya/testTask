da29b67ee1e184656d8318353b1a56c3a83461b8
Constantine <qostyab@gmail.com>
2015-04-24 05:18:53 +1000

    Comments, fixes

--- main.js
+++ main.js
@@ -3,27 +3,32 @@
     var CardsApp = {};
 
     CardsApp.refreshViewCards = function (arg) {
-        arg = arg || data;
-        var cardsHolder = document.getElementById('cards-holder');
-        cardsHolder.innerHTML = '';
+        arg = arg || data; // аргумент используется в основном history api для передачи актуального состояния массива
+        var cardsHolder = document.getElementById('cards-holder'),
+            cardsSafer = document.createDocumentFragment();
+        cardsHolder.innerHTML = ''; // удаляем старые карточки при обновлении массива
         for ( var i = 0; arg.length > i; i++ ) {
-            var card = document.createElement('li');
+            var card = document.createElement('li'); // добавляем элемент списка для карточки
             card.className += 'card card-' + arg[i].type;
-            if ( i === arg.length - 1 && arg.length != 1 ) {// добавление сдвига вправо, если карточек больше 1
-                card.className += ' card-first';
+            if ( i === arg.length - 1 && arg.length != 1 ) { // добавление сдвига вправо для последней
+                card.className += ' card-first';             // карточки, если их больше 1
             }
-            cardsHolder.appendChild( card );
+            cardsSafer.appendChild(card); // кэшируем для лучшего рендеринга
         }
+
+        cardsHolder.appendChild( cardsSafer ); // вставляем кэш с элементами
+        console.log('Boom');
     };
 
     CardsApp.hoverCards = function () {
-        var cardsHolder = document.getElementById('cards-holder');
-        var cards = document.querySelector('.cards');
-        var cardClasses = cards.className;
+        var cardsHolder = document.getElementById('cards-holder'),
+            cards = document.querySelector('.cards'),
+            cardClasses = cards.className; // кэшируем список классов
 
-        cardsHolder.onmouseenter = function () {
-            cards.className += ' cards-disable-hover';
-        };
+        cardsHolder.onmouseenter = function () {       // если наводишь на дочерний элемент блока .cards - .cards-holder,
+            cards.className += ' cards-disable-hover'; // то родительскому (cards) добавляется класс (cards-disable-hover),
+        };                                             // что меняет цвет его бэкграунда
+
         cardsHolder.onmouseleave = function () {
             cards.className = cardClasses;
         };
@@ -31,51 +36,49 @@
     };
 
     CardsApp.addCard = function (type) {
-        type = type || 'narrow';
+        type = type || 'narrow'; // если не задан тип карты, ставим по-умолчанию узкую
         data.push(
             {
                 type: type
             }
         );
-        this.refreshViewCards();
-        this.historyApi.add();
     };
 
     CardsApp.removeCard = function () {
-        if( data.length > 0 ) {
+        if ( data.length > 0 ) { // чтобы не пыталось удалить из пустого массива
             var cardsLength = data.length;
             data.splice(cardsLength - 1, 1);
-            this.historyApi.add();
-            this.refreshViewCards();
         }
     };
 
     CardsApp.editCards = function () {
         var cards = document.querySelector('.cards');
         cards.addEventListener("click", function(e) {
-            e = e || window.event;
-            if( e.shiftKey ) {
+            if ( e.shiftKey ) { // ловим нажатые кнопки
                 if ( e.altKey ) {
                     CardsApp.addCard('wide');
                 } else {
-                    CardsApp.addCard('narrow');
+                    CardsApp.addCard('narrow'); // если только шифт
                 }
             } else {
-                CardsApp.removeCard();
+                CardsApp.removeCard(); // если только клик
             }
-        }, false);
+
+            CardsApp.refreshViewCards(); // после каждого изменения обновляем вьюху
+            CardsApp.historyApi.add();   // и добавляем это изменение в хистори апи
+        });
     };
 
     CardsApp.historyApi = {
         add: function () {
-            if ( window.history ) {
-                history.pushState(data, null)
+            if ( window.history ) { // если поддерживается
+                history.pushState(data, null); // сохраняем в метод state актуальный массив
             }
         },
         detectChanges: function() {
-            window.addEventListener('popstate', function(e) {
-                data = e.state; // берем массив из истории и кладем его во вью
-                CardsApp.refreshViewCards(data);
+            window.addEventListener('popstate', function(e) { // ловим нажатия стрелок истории, передаем в аргумент данные перехода
+                data = e.state; // обновляем массив данными из истории
+                CardsApp.refreshViewCards(data); // используем его во вью
             });
         }
     };
