09038672c521522308199ba3049054773cec7919
Constantine <qostyab@gmail.com>
2015-04-23 20:09:11 +1000

    added HB.js (comments)

--- main.js
+++ main.js
@@ -8,18 +8,34 @@
 
     CardsApp.refreshViewCards = function () {
         CardsApp.cardsHolder.innerHTML = '';
-
-        for ( var i = 0; CardsApp.cards.length > i; i++ ) {
+        for ( var i = 0; data.length > i; i++ ) {
             var card = document.createElement('li');
-            var cardText = document.createTextNode( this.cards[i].type );
+            var cardText = document.createTextNode( data[i].type );
             card.appendChild( cardText );
-            card.className += 'card card-' + this.cards[i].type;
+            card.className += 'card card-' + data[i].type;
 
-            if ( i === this.cards.length - 1 && this.cards.length != 1 ) {// добавление сдвига вправо, если карточек больше 1
+            if ( i === data.length - 1 && data.length != 1 ) {// добавление сдвига вправо, если карточек больше 1
                 card.className += ' card-first';
             }
             this.cardsHolder.appendChild( card );
         }
+        console.log('refreshed');
+        /*var cardTemplate = document.getElementById('card-template').innerHTML;
+
+        for (var i = 0; data.length > i; i++) {
+            data[i].id = i;
+        }
+
+        var cardsData = {
+            posts: data
+        };
+
+        var templateHB = Handlebars.compile( cardTemplate );
+
+        CardsApp.cardsHolder.innerHTML = templateHB(cardsData);
+
+        Handlebars.registerHelper('id_', function(cd) {
+        });*/
     };
 
     CardsApp.hoverCards = function () {
@@ -37,8 +53,7 @@
 
     CardsApp.addCard = function (type) {
         type = type || 'narrow';
-
-        this.cards.push(
+        data.push(
             {
                 type: type
             }
@@ -47,9 +62,9 @@
     };
 
     CardsApp.removeCard = function () {
-        var cardsLength = CardsApp.cards.length;
-        if ( cardsLength >= 0 ) {
-            this.cards.splice(cardsLength - 1, 1);
+        if( data.length > 0 ) {
+            var cardsLength = data.length;
+            data.splice(cardsLength - 1, 1);
             this.refreshViewCards();
         }
     };
@@ -61,18 +76,17 @@
             if( e.shiftKey ) {
                 if ( e.altKey ) {
                     CardsApp.addCard('wide');
-                    return;
+                } else {
+                    CardsApp.addCard('narrow');
                 }
-
-                CardsApp.addCard('narrow');
             } else {
                 CardsApp.removeCard();
             }
         }, false);
+
+        CardsApp.refreshViewCards();
+        CardsApp.hoverCards();
     };
 
     CardsApp.editCards();
-    CardsApp.refreshViewCards();
-    CardsApp.removeCard();
-    CardsApp.hoverCards();
 }(cards));
